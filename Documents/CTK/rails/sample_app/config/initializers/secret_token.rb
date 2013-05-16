025d0ce49298297485b2997a45a38c51ae3ef945
Tobias Tikka <tikka@dhcp-184035.eduroam.chalmers.se>
2013-05-16 18:39:33 +0200

    Finish static pages

---
+++ Documents/CTK/rails/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9bfdce5db9031f078afad0fd6aef847511ebd438b8d99b2bb053a1d3a4a8b258a88e29cde38bece86693159bed77b81023adcea40d49ce9126c9fcab54e05644'
